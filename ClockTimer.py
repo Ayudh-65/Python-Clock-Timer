90bfb8da71ed99c015cfc0eee1ac30d2c882e336
Ayudh-65 <68161774+Ayudh-65@users.noreply.github.com>
2021-12-03 23:50:44 +0530

    Added beeping sound functionality

--- ClockTimer.py
+++ ClockTimer.py
@@ -1,17 +1,20 @@
 import time
+import winsound
 
+x = 0
 currentTime = time.strftime("%H:%M:%S")
 
 print("Current Time is:", currentTime)
 addTime = float(input("Set a timer for how many minutes?:"))
 
 currentTimeInt = float(time.strftime("%H%M%S"))
-timerEnd = (currentTimeInt + (addTime*100) - 1)
+timerEnd = (currentTimeInt + (addTime*100))
 
-print("Timer set for", addTime, "minutes!")
+print("Timer set for", addTime, "minutes.")
 
 while(int(time.strftime("%H%M%S")) < timerEnd):
     time.sleep(1)
 
 print("Your", addTime, "minutes are over!")
-x=input()
+winsound.PlaySound("Beeping.wav", winsound.SND_LOOP + winsound.SND_ASYNC)
+x=input("Press enter to stop the timer...")
