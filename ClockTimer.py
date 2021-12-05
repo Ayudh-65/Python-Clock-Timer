b51f277afa299b3f46758719c768617f74c594a9
Ayudh-65 <68161774+Ayudh-65@users.noreply.github.com>
2021-12-05 22:10:47 +0530

    Cleaned up code

--- ClockTimer.py
+++ ClockTimer.py
@@ -1,15 +1,12 @@
 import time
 import winsound
 
-x = 0
 currentTime = time.strftime("%H:%M:%S")
-
 print("Current Time is:", currentTime)
+
 addTime = float(input("Set a timer for how many minutes?:"))
 
-currentTimeInt = float(time.strftime("%H%M%S"))
-timerEnd = (currentTimeInt + (addTime*100))
-
+timerEnd = (float(time.strftime("%H%M%S")) + (addTime*100))
 print("Timer set for", addTime, "minutes.")
 
 while(int(time.strftime("%H%M%S")) < timerEnd):
@@ -17,4 +14,5 @@ while(int(time.strftime("%H%M%S")) < timerEnd):
 
 print("Your", addTime, "minutes are over!")
 winsound.PlaySound("Beeping.wav", winsound.SND_LOOP + winsound.SND_ASYNC)
+
 x=input("Press enter to stop the timer...")
